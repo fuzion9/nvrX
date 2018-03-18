eb3e04a718007dc1d36abc2c30c5041bfa406e11
fuzion9 <fuzion9@gmail.com>
2018-03-18 12:29:29 -0400

    Add system monitoring

---
+++ src/app/system-stats/system-stats.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {StreamControlService} from "../stream-control.service";
+
+@Component({
+  selector: 'app-system-stats',
+  templateUrl: './system-stats.component.html',
+  styleUrls: ['./system-stats.component.css']
+})
+export class SystemStatsComponent implements OnInit, AfterViewInit {
+    stats = {};
+    guageType = 'semi';
+    guageSize = 75;
+    guages:any = {mem:{}, cpu:{}};
+
+  constructor(private scs:StreamControlService) { }
+
+  ngOnInit() {
+      this.scs.getSystemStats();
+      this.scs.lastSystemStats.subscribe((data)=>{
+          console.log(data);
+          if (data.totalMem) {
+              this.processData(data);
+          }
+
+      })
+  }
+  ngAfterViewInit(){
+      let labels = document.getElementsByClassName('reading-label');
+      for (let i=0; i < labels.length; i++){
+          let e = <HTMLElement>labels[i];
+          e.style.fontSize='8px';
+          e.style.lineHeight='85px'
+      }
+      labels = document.getElementsByClassName('reading-block');
+      for (let i=0; i < labels.length; i++){
+          let e = <HTMLElement>labels[i];
+          e.style.lineHeight='55px';
+      }
+  }
+
+  processData(data){
+      let mem = data.totalMem.split(' ');
+      let memfree = data.freeMem.split(' ');
+      this.guages.mem.totalMem = parseFloat(mem[0]);
+      this.guages.mem.usedMem = this.guages.mem.totalMem - memfree[0];
+      this.guages.mem.append = mem[1];
+      this.guages.mem.label = 'of ' + data.totalMem;
+
+      this.guages.cpu.label = 'CPU Usage';
+      this.guages.cpu.append = '%';
+      this.guages.cpu.value = parseInt(data.cpuPercent);
+      console.log(data);
+
+  }
+
+}
